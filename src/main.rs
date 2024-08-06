229b2a1af6f581c0f022313745851f2c2b91312b
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-08-06 09:56:04 +0200

    add todos on main
    I need to remember all those things in axum

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,26 @@ use axum::{routing::get, Router};
 
 use website::view::{home, root};
 
+// TODO: impl a state
+// TODO: impl a extension
+// TODO: impl a extractor
+// TODO: impl a middleware
+
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    tracing_subscriber::fmt().init();
     let port = "3000";
-
+    tracing_subscriber::fmt().init();
     tracing::info!("router initialized, now listening on port {}", port);
 
     let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{port}"))
         .await
         .unwrap();
 
-    let app = Router::new()
+    let pages = Router::new()
         .route("/", get(root))
         .route("/home", get(home));
+
+    let app = Router::new().nest(":i18n", pages);
 
     axum::serve(listener, app).await.unwrap();
 
