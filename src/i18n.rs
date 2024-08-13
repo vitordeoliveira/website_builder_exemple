525b4b6f2fdda048949454f8fbfd89d267e0332c
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-08-13 17:30:58 +0200

    refactor on i18n

--- src/i18n.rs
+++ src/i18n.rs
@@ -1,76 +1,25 @@
-use std::{collections::HashSet, fmt::Display, str::FromStr, sync::Arc};
+use std::str::FromStr;
 
-use axum::{extract::Request, middleware::Next, response::IntoResponse, Extension};
-use strum::{EnumIter, IntoEnumIterator};
+use axum::{extract::Request, middleware::Next, response::IntoResponse};
+use strum::{Display, EnumString};
 
 // TODO: should be a struct that impl Display
 // TODO: should work in a way that the translations should be set IN the file that use it
 
-#[derive(Clone, Debug)]
-pub struct AllowedLangs {
-    pub langs: Arc<HashSet<String>>,
-    pub default_lang: Lang,
-}
-
-pub async fn i18n_middleware(
-    Extension(allowed_langs): Extension<AllowedLangs>,
-    mut req: Request,
-    next: Next,
-) -> Result<impl IntoResponse, ()> {
-    let test = Lang::as_vec();
-    println!("{test:?}");
-
-    let path = req.uri().path().to_string();
-    let lang_segment = path.trim_start_matches('/').split('/').next().unwrap();
-
-    let lang = Lang::from_str(lang_segment)?;
-
-    let lang = if allowed_langs.langs.contains(lang.as_str()) {
-        lang
-    } else {
-        allowed_langs.default_lang
-    };
-
-    req.extensions_mut().insert(lang);
-    Ok(next.run(req).await)
-}
-
-#[derive(Clone, Debug, EnumIter, PartialEq)]
+#[derive(Clone, EnumString, Display)]
+#[strum(ascii_case_insensitive)]
 pub enum Lang {
     En,
     Fr,
     Es,
 }
 
-impl Lang {
-    pub fn as_str(&self) -> &str {
-        match self {
-            Lang::En => "en",
-            Lang::Fr => "fr",
-            Lang::Es => "es",
-        }
-    }
+pub async fn i18n_middleware(mut req: Request, next: Next) -> Result<impl IntoResponse, ()> {
+    let path = req.uri().path().to_string();
+    let lang_segment = path.trim_start_matches('/').split('/').next().unwrap();
 
-    pub fn as_vec() -> Vec<String> {
-        Lang::iter().map(|e| e.to_string()).collect::<Vec<String>>()
-    }
-}
+    let lang = Lang::from_str(lang_segment).unwrap_or(Lang::En);
 
-impl FromStr for Lang {
-    type Err = ();
-
-    fn from_str(lang: &str) -> Result<Self, Self::Err> {
-        Ok(match lang {
-            "en" => Lang::En,
-            "fr" => Lang::Fr,
-            "es" => Lang::Es,
-            _ => Lang::En,
-        })
-    }
-}
-
-impl Display for Lang {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
+    req.extensions_mut().insert(lang);
+    Ok(next.run(req).await)
 }
