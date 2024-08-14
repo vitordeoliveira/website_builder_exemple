0a5787c5796296b66a17ce0e1d2eef2bd162c4e2
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-08-14 11:57:23 +0200

    remove exemples parameters from root exemple page

--- src/view/root/mod.rs
+++ src/view/root/mod.rs
@@ -5,7 +5,7 @@ use axum::{
     response::{Html, IntoResponse},
 };
 
-use crate::{i18n::I18N, AppState, ExtractTitle};
+use crate::{i18n::I18N, AppState};
 
 #[derive(Template)]
 #[template(path = "root/root.html")]
@@ -13,15 +13,8 @@ pub struct RootTemplate {
     title: String,
 }
 
-pub async fn root(
-    i18n: I18N,
-    State(AppState { title }): State<AppState>,
-    ExtractTitle(extract_title): ExtractTitle,
-) -> impl IntoResponse {
-    let title = format!(
-        "from state: {} ---- from extractor {} ---- lang_from_extractor:{i18n}",
-        title, extract_title
-    );
+pub async fn root(i18n: I18N, State(AppState { title }): State<AppState>) -> impl IntoResponse {
+    let title = format!("from state: {} ---- lang_from_extractor:{i18n}", title);
     let root = RootTemplate { title };
     (StatusCode::OK, Html(root.render().unwrap()))
 }
