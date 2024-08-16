e9f5403126fbe49c64557ae9c8e79bba87c6bd4e
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-08-16 21:28:43 +0200

    I18N

--- src/error.rs
+++ src/error.rs
@@ -5,16 +5,26 @@ use axum::{
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
-pub enum SysError {
-    #[error("I18N Error: {0}")]
-    I18NError(String),
-}
+pub enum SysError {}
 
 impl IntoResponse for SysError {
     fn into_response(self) -> axum::response::Response {
+        // error!("{self}");
+        // match self {
+        //     SysError::I18NError(path) => {
+        //         let mut segments = path
+        //             .trim_start_matches('/')
+        //             .splitn(2, '/')
+        //             .collect::<Vec<&str>>();
+        //         segments[0] = "en";
+        //         let new_path = format!("/{}", segments.join("/"));
+        //         Redirect::temporary(&new_path).into_response()
+        //     }
+        // }
+
         (
             StatusCode::INTERNAL_SERVER_ERROR,
-            Html("INTERNAL_SERVER_ERROR".to_string()),
+            Html("internal server error"),
         )
             .into_response()
     }
--- src/i18n/mod.rs
+++ src/i18n/mod.rs
@@ -8,13 +8,25 @@ use strum::{Display, EnumString};
 
 use crate::error::SysError;
 
-#[derive(Clone, EnumString, Display)]
+#[derive(Clone, EnumString, Display, PartialEq, Debug)]
 #[strum(ascii_case_insensitive)]
 pub enum I18N {
     En,
     Fr,
     Es,
     De,
+}
+
+impl I18N {
+    fn get_path_language(path: String) -> Self {
+        let lang_segment = path
+            .trim_start_matches('/')
+            .split('/')
+            .next()
+            .unwrap_or("invalid");
+
+        I18N::from_str(lang_segment).unwrap_or(I18N::En)
+    }
 }
 
 #[async_trait]
@@ -26,32 +38,31 @@ where
 
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
         let path = parts.uri.to_string();
-        let lang_segment = path
-            .trim_start_matches('/')
-            .split('/')
-            .next()
-            .unwrap_or("en");
-
-        let lang = I18N::from_str(lang_segment).map_err(|e| SysError::I18NError(e.to_string()))?;
-
-        Ok(lang)
+        Ok(Self::get_path_language(path))
     }
 }
 
 #[cfg(test)]
-#[test]
-fn should_return_i18n_error() {
-    let lang_segment = "invalid_lang";
-    let result = I18N::from_str(lang_segment).map_err(|e| SysError::I18NError(e.to_string()));
+mod tests {
+    use crate::i18n::I18N;
 
-    match result {
-        Err(err_msg) => {
-            assert!(err_msg
-                .to_string()
-                .contains("I18N Error: Matching variant not found"));
-        }
-        Ok(_) => {
-            panic!("Expected SysError::I18NError, got something else.");
-        }
+    #[test]
+    fn should_return_default_en_on_path() {
+        assert_eq!(I18N::get_path_language("fakepath".to_string()), I18N::En)
+    }
+
+    #[test]
+    fn should_return_german_on_path() {
+        assert_eq!(I18N::get_path_language("/de".to_string()), I18N::De)
+    }
+
+    #[test]
+    fn should_return_french_on_path() {
+        assert_eq!(I18N::get_path_language("/fr".to_string()), I18N::Fr)
+    }
+
+    #[test]
+    fn should_return_spanish_on_path() {
+        assert_eq!(I18N::get_path_language("/es".to_string()), I18N::Es)
     }
 }
