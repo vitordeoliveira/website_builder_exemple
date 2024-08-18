fe735a677abec6e718854bdca5274af077edc68e
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-08-18 14:39:33 +0200

    basic impl of opentelemetry with local jeager (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use anyhow::Result;
 use axum::{routing::get, Router};
 
+use opentelemetry::{
+    trace::{TraceError, TracerProvider as _},
+    KeyValue,
+};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::trace as sdktrace;
+use opentelemetry_sdk::{runtime, Resource};
+use opentelemetry_semantic_conventions::resource::SERVICE_NAME;
+use tracing::instrument;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use website::view::{home, root};
-
 // TODO: setup tracing with open telemetry
 // TODO: setup config singleton
 // TODO: impl adatper for database connections
@@ -15,11 +24,44 @@ use website::view::{home, root};
 // TODO: write blogpost about tracing with open telemetry
 // TODO: write blogpost about snapshot testing with askama
 
+fn init_tracer_provider() -> Result<opentelemetry_sdk::trace::TracerProvider, TraceError> {
+    opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("http://localhost:4317"),
+        )
+        .with_trace_config(
+            sdktrace::Config::default().with_resource(Resource::new(vec![KeyValue::new(
+                SERVICE_NAME,
+                "website-builder",
+            )])),
+        )
+        .install_batch(runtime::Tokio)
+}
+
 #[tokio::main]
+#[instrument]
 async fn main() -> Result<()> {
     let port = "3000";
-    tracing_subscriber::fmt().init();
-    tracing::info!("router initialized, now listening on port {}", port);
+    // Create a new OpenTelemetry trace pipeline that prints to stdout
+    // let provider = TracerProvider::builder()
+    //     .with_simple_exporter(stdout::SpanExporter::default())
+    //     .build();
+
+    let tracer = init_tracer_provider().unwrap().tracer("my_app");
+
+    // Create a tracing layer with the configured tracer
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(telemetry)
+        .init();
+
+    tracing::trace!("router initialized, now listening on port {}", port);
+    // tracing::error!("router initialized, now listening on port {}", port);
 
     let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{port}"))
         .await
