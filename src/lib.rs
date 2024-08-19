92bd6f3ff879489c83fb7956e923c20a32689995
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-08-19 11:54:14 +0200

    remove unused appstate and add mod config

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,8 @@
 // region
 // flatten
 // public
+pub mod config;
 pub mod controller;
 pub mod error;
 pub mod i18n;
 pub mod view;
-
-#[derive(Clone, Debug)]
-pub struct AppState {
-    pub title: &'static str,
-}
