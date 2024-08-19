4916e20d2a2f0ea23050521bde61aca1e421e4c1
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-08-19 11:52:21 +0200

    move tracing setup to config folder

---
+++ src/config/tracing.rs
@@ -0,0 +1,65 @@
+use anyhow::{Ok, Result};
+use opentelemetry::{trace::TracerProvider as _, KeyValue};
+use opentelemetry_sdk::trace::{RandomIdGenerator, Sampler, Tracer};
+use opentelemetry_sdk::{runtime, Resource};
+use opentelemetry_semantic_conventions::resource::{
+    DEPLOYMENT_ENVIRONMENT, SERVICE_NAME, SERVICE_VERSION,
+};
+use opentelemetry_semantic_conventions::SCHEMA_URL;
+use tracing::level_filters::LevelFilter;
+use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+pub struct Tracing;
+
+fn resource() -> Resource {
+    Resource::from_schema_url(
+        [
+            KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
+            KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
+            KeyValue::new(DEPLOYMENT_ENVIRONMENT, "develop"),
+        ],
+        SCHEMA_URL,
+    )
+}
+
+fn init_tracer_provider() -> Result<Tracer> {
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter().tonic(), // .with_endpoint("http://localhost:4317"),
+        )
+        .with_trace_config(
+            opentelemetry_sdk::trace::Config::default()
+                // Customize sampling strategy
+                .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
+                    1.0,
+                ))))
+                // If export trace to AWS X-Ray, you can use XrayIdGenerator
+                .with_id_generator(RandomIdGenerator::default())
+                .with_resource(resource()),
+        )
+        .install_batch(runtime::Tokio)?
+        .tracer("tracing-otel-subscriber");
+
+    Ok(tracer)
+}
+
+// To help impl https://github.com/tokio-rs/tracing-opentelemetry/blob/v0.1.x/examples/opentelemetry-otlp.rs
+// fn init_metrics_provider() -> Result<SdkMeterProvider> {
+//     todo!()
+// }
+
+impl Tracing {
+    pub fn setup() -> Result<()> {
+        let tracer = init_tracer_provider()?;
+
+        tracing_subscriber::registry()
+            .with(tracing_subscriber::fmt::layer())
+            .with(OpenTelemetryLayer::new(tracer))
+            .with(LevelFilter::DEBUG)
+            .init();
+
+        Ok(())
+    }
+}
