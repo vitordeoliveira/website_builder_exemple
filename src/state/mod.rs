2f0b53b9e6397beb743dcf80d3171939db814fee
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-08-22 15:26:35 +0200

    AppState

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,6 +1,23 @@
-use sqlx::PgPool;
+use std::time::Duration;
+
+use anyhow::Context;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 
 #[derive(Clone, Debug)]
 pub struct AppState {
     pub pg_pool: PgPool,
+}
+
+impl AppState {
+    pub async fn new(db_connection_str: &str) -> Self {
+        let pg_pool = PgPoolOptions::new()
+            .max_connections(25)
+            .acquire_timeout(Duration::from_secs(3))
+            .connect(db_connection_str)
+            .await
+            .context("postgres database pool creation error")
+            .expect("can't connect to database");
+
+        Self { pg_pool }
+    }
 }
