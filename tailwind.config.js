079c87dab9545a6d9f87feb55008dbe454cc0d9e
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-08-23 12:52:13 +0200

    tailwind deps

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/view_controller/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "selector",
+}
+
