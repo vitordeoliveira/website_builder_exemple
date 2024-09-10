e71f022b0c5a3d66c1b6f9511d931d43e1cd1bd2
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-09-10 16:50:00 +0100

    a little bit better, but still not good enough

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,4 +1,6 @@
-use std::sync::{Arc, Mutex};
+// use std::sync::{Arc, Mutex, MutexGuard};
+
+use std::sync::RwLock;
 
 use axum_test::TestServer;
 use ctor::dtor;
@@ -8,12 +10,37 @@ use website::app::new_app;
 
 use testcontainers::{core::WaitFor, runners::AsyncRunner, ContainerAsync, GenericImage, ImageExt};
 
-type PostgresContainer = OnceCell<ContainerAsync<GenericImage>>;
+// type PostgresContainer = OnceCell<ContainerAsync<GenericImage>>;
+//
+// static INSTANCE_POSTGRES: PostgresContainer = OnceCell::const_new();
+// pub async fn postgres() -> &'static ContainerAsync<GenericImage> {
+//     INSTANCE_POSTGRES
+//         .get_or_init(|| async {
+//             GenericImage::new("postgres", "14.1")
+//                 .with_wait_for(WaitFor::message_on_stdout(
+//                     "database system is ready to accept connections",
+//                 ))
+//                 .with_wait_for(WaitFor::seconds(5))
+//                 .with_env_var("POSTGRES_USER", "test_user")
+//                 .with_env_var("POSTGRES_PASSWORD", "test_password")
+//                 .with_env_var("POSTGRES_DB", "test_db")
+//                 .with_container_name("integration_test_postgres")
+//                 .start()
+//                 .await
+//                 .expect("Postgres start")
+//         })
+//         .await
+// }
 
-static INSTANCE_POSTGRES: PostgresContainer = OnceCell::const_new();
-pub async fn postgres() -> &'static ContainerAsync<GenericImage> {
-    INSTANCE_POSTGRES
-        .get_or_init(|| async {
+static STATIC_INSTANCE: Lazy<RwLock<Option<ContainerAsync<GenericImage>>>> = Lazy::new(|| {
+    RwLock::new(None) // Initialize with None
+});
+
+async fn init() {
+    let mut instance = STATIC_INSTANCE.write().unwrap();
+
+    if instance.is_none() {
+        *instance = Some(
             GenericImage::new("postgres", "14.1")
                 .with_wait_for(WaitFor::message_on_stdout(
                     "database system is ready to accept connections",
@@ -22,74 +49,48 @@ pub async fn postgres() -> &'static ContainerAsync<GenericImage> {
                 .with_env_var("POSTGRES_USER", "test_user")
                 .with_env_var("POSTGRES_PASSWORD", "test_password")
                 .with_env_var("POSTGRES_DB", "test_db")
-                .with_container_name("integration_test_postgres")
                 .start()
                 .await
-                .expect("Postgres start")
-        })
-        .await
+                .expect("Postgres start"),
+        );
+    };
 }
 
-static STATIC_INSTANCE: Lazy<Arc<Mutex<Option<ContainerAsync<GenericImage>>>>> = Lazy::new(|| {
-    Arc::new(Mutex::new(None)) // Initialize with None
-});
-
-async fn get_or_init() -> Option<ContainerAsync<GenericImage>> {
-    let mut instance = STATIC_INSTANCE.lock().unwrap(); // Lock the mutex
-    if instance.is_none() {
-        let init = GenericImage::new("postgres", "14.1")
-            .with_wait_for(WaitFor::message_on_stdout(
-                "database system is ready to accept connections",
-            ))
-            .with_wait_for(WaitFor::seconds(5))
-            .with_env_var("POSTGRES_USER", "test_user")
-            .with_env_var("POSTGRES_PASSWORD", "test_password")
-            .with_env_var("POSTGRES_DB", "test_db")
-            .start()
-            .await
-            .expect("Postgres start");
-        *instance = Some(init); // Initialize if it's None
-        println!("Instance initialized");
-        instance.take()
-    } else {
-        println!("Instance was already initialized");
-        instance.take()
-    }
-}
-
+//
 #[dtor]
 fn drop_instance() {
     println!("Program terminating, dropping static instance...");
 
-    let mut instance = STATIC_INSTANCE.lock().unwrap();
-    if instance.take().is_some() {
-        println!("Dropping value"); // Clean up the resource
-    } else {
-        println!("Nothing to drop, instance was not initialized.");
-    }
-}
+    let container = STATIC_INSTANCE.read().unwrap();
 
-// #[dtor]
-// fn shutdown() {
-//     std::process::Command::new("docker")
-//         .arg("kill")
-//         .arg("integration_test_postgres")
-//         .output()
-//         .expect("failed to kill container");
-//
-//     std::process::Command::new("docker")
-//         .arg("rm")
-//         .arg("integration_test_postgres")
-//         .output()
-//         .expect("failed to kill container");
-// }
+    let id = container.as_ref().unwrap().id();
+
+    std::process::Command::new("docker")
+        .arg("kill")
+        .arg(id)
+        .output()
+        .expect("failed to kill container");
+
+    std::process::Command::new("docker")
+        .arg("rm")
+        .arg(id)
+        .output()
+        .expect("failed to kill container");
+}
 
 pub async fn setup() -> TestServer {
     // let container = postgres().await;
 
-    let container = get_or_init().await.unwrap();
+    let _ = init().await;
 
-    let port = container.get_host_port_ipv4(5432).await.unwrap();
+    let container = STATIC_INSTANCE.read().unwrap();
+
+    let port = container
+        .as_ref()
+        .unwrap()
+        .get_host_port_ipv4(5432)
+        .await
+        .unwrap();
 
     let db_connection = format!("postgres://test_user:test_password@localhost:{port}/test_db");
     let assets_path = env!("CARGO_MANIFEST_DIR");
