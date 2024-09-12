0f4eddf05f757271f9f37968bfca3e86c3d239eb
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-09-12 09:14:12 +0100

    change / to home instead of root

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,14 @@
-use crate::{
-    view_controller::{home, root},
-    AppState,
-};
+use crate::{view_controller::home, AppState};
 use anyhow::Result;
 use axum::{routing::get, Router};
 use tower_http::services::ServeDir;
 
 pub async fn new_app(db_connection: &str, assets_path: &str) -> Result<axum::Router> {
     let app_state = AppState::new(db_connection).await?;
-    let translated_pages = Router::new()
-        .route("/:i18n", get(root))
-        .route("/:i18n/home", get(home));
+    let translated_pages = Router::new().route("/:i18n", get(home));
 
     Ok(Router::new()
-        .route("/", get(root))
+        .route("/", get(home))
         .merge(translated_pages)
         .with_state(app_state)
         .nest_service("/assets", ServeDir::new(format!("{}/assets", assets_path))))
